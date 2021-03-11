0cb20d91842d31d12c928f659b0ad8892b943028
Oleg Muhachev <olmu1996@gmail.com>
2021-03-11 17:12:43 +0300

    add "filter" feature

--- src/App.js
+++ src/App.js
@@ -13,26 +13,62 @@ let inputText;
 
 const Form = () => {
   const [itemList, setItemList] = useState([]);
+  const [filter, setFilter] = useState('all');
+  let Items;
   const Input = (e) => {
     if (e.key === 'Enter') {
-      inputText = document.querySelector('#inputKey').value;
-      setItemList([...itemList, { text: inputText, key: i++, ready: false }]);
-      document.querySelector('#inputKey').value = '';
+      inputText = document.querySelector('#inputKey').value
+      if (inputText.replace(/\s+/g, '').length === 0) {
+        inputText = 'Your empty ToDo =)'
+      }
+      setItemList([...itemList, { text: inputText, key: i++, ready: false }])
+      document.querySelector('#inputKey').value = ''
 
     }
   }
 
-  const Items = itemList.map((item) => {
-    return (
-      <div className="flexDiv" key={item.key}>
-        <div className="checkItem">
-          <input type="checkbox" className="Checker" onClick={() => setChecked(item.key)} />
+  switch (filter) {
+    case 'all': Items = itemList.map((item) => {
+      return (
+        <div className="flexDiv" key={item.key}>
+          <div className="checkItem">
+            <input type="checkbox" className="Checker" onClick={() => setChecked(item.key)} checked={item.ready} />
+          </div>
+          {item.ready === true ? <div className="Item" style={{ textDecoration: 'line-through' }}>{item.text}</div> : <div className="Item">{item.text}</div>}
+          <Icon className="Icon" onClick={() => itemDrop(item.key)} />
         </div>
-        {item.ready === true ? <div className="Item" style={{ textDecoration: 'line-through' }}>{item.text}</div> : <div className="Item">{item.text}</div>}
-        <Icon className="Icon" onClick={() => itemDrop(item.key)} />
-      </div>
-    )
-  })
+      )
+    });
+      break;
+    case 'todo': Items = itemList.map((item) => {
+      if (item.ready === false) {
+        return (
+          <div className="flexDiv" key={item.key}>
+            <div className="checkItem">
+              <input type="checkbox" className="Checker" onClick={() => setChecked(item.key)} checked={item.ready} />
+            </div>
+            {item.ready === true ? <div className="Item" style={{ textDecoration: 'line-through' }}>{item.text}</div> : <div className="Item">{item.text}</div>}
+            <Icon className="Icon" onClick={() => itemDrop(item.key)} />
+          </div>
+        )
+      }
+    });
+      break;
+    case 'complete': Items = itemList.map((item) => {
+      if (item.ready === true) {
+        return (
+          <div className="flexDiv" key={item.key}>
+            <div className="checkItem">
+              <input type="checkbox" className="Checker" onClick={() => setChecked(item.key)} checked={item.ready} />
+            </div>
+            {item.ready === true ? <div className="Item" style={{ textDecoration: 'line-through' }}>{item.text}</div> : <div className="Item">{item.text}</div>}
+            <Icon className="Icon" onClick={() => itemDrop(item.key)} />
+          </div>
+        )
+      }
+    });
+      break;
+  }
 
   const setChecked = (itemKey) => {
     setItemList(itemList.map(item => {
@@ -41,11 +77,28 @@ const Form = () => {
       }
       return item;
     }));
-    console.log(itemList);
   }
 
   const itemDrop = (itemKey) => {
     setItemList(itemList.filter(item => item.key !== itemKey))
+  }
+
+  const clearCompleted = () => {
+    setItemList(itemList.filter(item => !item.ready))
+  }
+
+  function Filter(filter, allStyle, todoStyle, completeStyle) {
+    setFilter(filter);
+    document.querySelector('#all').className = allStyle;
+    document.querySelector('#todo').className = todoStyle;
+    document.querySelector('#complete').className = completeStyle;
+  }
+
+  const checkAll = () => {
+    setItemList(itemList.map(item => {
+      item.ready = true
+      return item;
+    }));
   }
 
   return (
@@ -57,13 +110,13 @@ const Form = () => {
           {Items}
         </div>
         <div className="lowerButtons">
-          <div className="text">{itemList.length} tasks left</div>
+          <div className="text" onClick={checkAll}>{itemList.length} tasks left</div>
           <div>
-            <button className="btns">All</button>
-            <button className="btns">ToDo</button>
-            <button className="btns">Complete</button>
+            <button id="all" className="btns" onClick={() => Filter('all','btns-on','btns','btns')}>All</button>
+            <button id="todo" className="btns" onClick={() => Filter('todo','btns','btns-on','btns')}>ToDo</button>
+            <button id="complete" className="btns" onClick={() => Filter('complete','btns','btns','btns-on')}>Complete</button>
           </div>
-          <div className="text" id="textKey">Clear completed</div>
+          <div className="text" onClick={clearCompleted}>Clear completed</div>
         </div>
       </div>
     </React.Fragment>
