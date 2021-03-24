e01f9c58bb8f679b2dd23df2ddd8ab8708f114b3
Oleg Muhachev <olmu1996@gmail.com>
2021-03-24 17:45:10 +0300

    saving current filter in localStorage

--- src/store/actions/filter.js
+++ src/store/actions/filter.js
@@ -3,6 +3,9 @@ import { SET_FILTER } from "../constains/filter";
 export function setFilter(filter) {
     return function (dispatch) {
         try {
+            const localState = JSON.parse(localStorage.getItem('state'));
+            localState.filter.currentFilter = filter;
+            localStorage.setItem('state', JSON.stringify(localState));
             return dispatch({
                 type: SET_FILTER,
                 payload: filter,
